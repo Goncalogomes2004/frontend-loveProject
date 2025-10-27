ba4b76a34cb95c59e9f999d32b88358f984e679e
Gonçalo Gomes <goncalocardosogomes@gmail.com>
2025-10-27 23:58:15 +0000

    mobile app

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,43 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "logoWithText.png",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Framelove",
+        short_name: "Framelove",
+        description: "Repositório de fotos com amor!",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0f172a",
+        icons: [
+          {
+            src: "/logoWithText.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logoWithText.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
